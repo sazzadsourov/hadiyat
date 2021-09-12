0d45d24565b41d8db24e6f815fdbd91c5220cd98
sazzadsourov <84804356+sazzadsourov@users.noreply.github.com>
2021-09-13 02:15:51 +0600

    add js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.autoplay').slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });

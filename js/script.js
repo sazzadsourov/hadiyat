925c9d78790080b7a8d5f8a968e54bd2de8cb3bd
sazzadsourov <84804356+sazzadsourov@users.noreply.github.com>
2021-09-17 19:36:33 +0600

    add js & jQ

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
+// aos
+AOS.init({
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 300, // offset (in px) from the original trigger point
+    delay: 1, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  
+  });
+
+ 
+  wow = new WOW(
+    {
+    boxClass:     'wow',      // default
+    animateClass: 'animated', // default
+    offset:       0,          // default
+    mobile:       true,       // default
+    live:         true        // default
+  }
+  )
+  wow.init();
+
 $(document).ready(function(){
-    $('.autoplay').slick({
-        dots: true,
-        arrows: true,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    });
+
   });
